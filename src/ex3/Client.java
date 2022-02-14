67f707c4fcf5182ddce771f1cadc867e7030bd06
Juan David Murillo <juancho20sp@gmail.com>
2022-02-14 12:13:05 -0500

    AREP-02: Ex. 3 done

---
+++ src/ex3/Client.java
@@ -0,0 +1,44 @@
+package ex3;
+
+import java.io.*;
+import java.net.*;
+
+public class Client {
+    public static void main(String[] args) {
+        Socket socket = null;
+        PrintWriter out = null;
+        BufferedReader in = null;
+
+        try {
+            socket = new Socket("127.0.0.1", 1234);
+
+            System.out.println("Socket ready for sending data...");
+
+            out = new PrintWriter(socket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+            System.exit(1);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+        String clientInput;
+
+        try {
+            while ((clientInput = stdIn.readLine()) != null) {
+                out.println(clientInput);
+                System.out.println(in.readLine());
+            }
+
+            out.close();
+            in.close();
+            stdIn.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/ex3/Server.java
@@ -0,0 +1,56 @@
+package ex3;
+
+import java.io.*;
+import java.net.*;
+
+public class Server {
+    public static void main(String[] args) {
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
+
+        final int PORT = 1234;
+
+        try {
+            serverSocket = new ServerSocket(PORT);
+
+            System.out.println("Server listening on port: " + PORT);
+            System.out.println("Waiting for connections...");
+
+            clientSocket = serverSocket.accept();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+
+        try {
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String inputLine, outputLine;
+
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println("Mensaje: " + inputLine);
+
+                try {
+                    double number = Double.parseDouble(inputLine);
+                    outputLine = "Respuesta: " + number * number;
+                } catch (NumberFormatException ex) {
+                    outputLine = "Respuesta: " + inputLine;
+                }
+
+                out.println(outputLine);
+
+                if (outputLine.equals("Respuesta: Bye.")) {
+                    break;
+                }
+            }
+
+            out.close();
+            in.close();
+            clientSocket.close();
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
